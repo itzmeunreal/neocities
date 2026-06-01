4a16bbe036171322fe062bee48406d66f0faa3f3
unreal! <aimaressi88@gmail.com>
2026-06-01 03:00:25 +0200

    Create emu.js

---
+++ nespad/emu.js
@@ -0,0 +1,43 @@
+EJS_player = "#game";
+EJS_core = "nestopia";
+EJS_gameName = "super mario bros.";
+EJS_color = "#e52222";
+EJS_startOnLoaded = true;
+EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+EJS_gameUrl = "https://itzmeunreal.github.io/neocities/rom/Super%20Mario%20Bros.%20(Japan%2C%20USA).nes";
+EJS_disableLocalStorage = true;
+EJS_defaultControls = {
+    0: {
+        0: {value: 'left arrow', value2: 'BUTTON_2'},
+        1: {value: '', value2: 'BUTTON_4'},
+        2: {value: 'backspace', value2: 'SELECT'},
+        3: {value: 'enter', value2: 'START'},
+        4: {value: 'w', value2: 'DPAD_UP'},
+        5: {value: 's', value2: 'DPAD_DOWN'},
+        6: {value: 'a', value2: 'DPAD_LEFT'},
+        7: {value: 'd', value2: 'DPAD_RIGHT'},
+        8: {value: 'down arrow', value2: 'BUTTON_1'},
+        9: {value: '', value2: 'BUTTON_3'},
+        10: {value: '', value2: 'LEFT_TOP_SHOULDER'},
+        11: {value: '', value2: 'RIGHT_TOP_SHOULDER'},
+        12: {value: '', value2: 'LEFT_BOTTOM_SHOULDER'},
+        13: {value: '', value2: 'RIGHT_BOTTOM_SHOULDER'},
+        14: {value: '', value2: 'LEFT_STICK'},
+        15: {value: '', value2: 'RIGHT_STICK'},
+        16: {value: '', value2: 'LEFT_STICK_X:+1'},
+        17: {value: '', value2: 'LEFT_STICK_X:-1'},
+        18: {value: '', value2: 'LEFT_STICK_Y:+1'},
+        19: {value: '', value2: 'LEFT_STICK_Y:-1'},
+        20: {value: '', value2: 'RIGHT_STICK_X:+1'},
+        21: {value: '', value2: 'RIGHT_STICK_X:-1'},
+        22: {value: '', value2: 'RIGHT_STICK_Y:+1'},
+        23: {value: '', value2: 'RIGHT_STICK_Y:-1'},
+        24: {value: '1'},
+        25: {value: '2'},
+        26: {value: '3'},
+        27: {value: 'add'},
+        28: {value: 'space'},
+        29: {value: 'subtract'}
+    },
+    1: {}, 2: {}, 3: {}
+};

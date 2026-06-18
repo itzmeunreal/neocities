6e80c83968fccc9a2dcbe6bba9231702f7b75d0c
unreal! <aimaressi88@gmail.com>
2026-06-18 03:28:25 +0200

    Add files via upload

---
+++ music/playlist-gingus.js
@@ -0,0 +1,146 @@
+window.playlistData = {
+	name: "gingus",
+	pfp: "https://itzmeunreal.neocities.org/images/covers/gingus/gingus.jpg",
+	description: "this guy can make an album full of half a min songs and then drop a fucking 7 minute long single",
+	sectionName: "gingus",
+	tracks: [
+		{
+			title: "snowpoint city",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/snowpoint%20city.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "2008",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/2008.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "homeswitcher",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/homeswitcher.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "newmoon island",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/newmoon%20island.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "hit the quan",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/hit%20the%20quan.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "hahaha DIE",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/yellow/hahaha%20DIE.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/yellow.jpg"
+		},
+		{
+			title: "erm actually ladies booty bounce to this",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/erm%20actually%20ladies%20booty%20bounce%20to%20this.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "i can make giratina rhyme with dingus",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/i%20can%20make%20giratina%20rhyme%20with%20dingus.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "everyone SHUT UP. i have something to say...",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/everyone%20SHUT%20UP.%20i%20have%20something%20to%20say....mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "when you hear it you'll shit bricks",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/when%20you%20hear%20it%20you'll%20shit%20bricks.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "to derp or not to derp",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/to%20derp%20or%20not%20to%20derp.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "that's just the difference between you and me",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/that's%20just%20the%20difference%20between%20you%20and%20me.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "all i wanna do is ditch them",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/emerald/all%20i%20wanna%20do%20is%20ditch%20them.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/emerald.jpg"
+		},
+		{
+			title: "DURST DAY",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/DURST%20DAY.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "BONErr",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/BONErr.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "teach me how to dougie",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/teach%20me%20how%20to%20dougie.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "i have a little fun",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/i%20have%20a%20little%20fun.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "im making music friends",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/im%20making%20music%20friends.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "the bitch phone rang, its for you",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/the%20bitch%20phone%20rang,%20its%20for%20you.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "giratina anthem",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/giratina%20anthem.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "dont say \"i can sample that\" for 24 hours challenge",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/dont%20say%20_i%20can%20sample%20that_%20for%2024%20hours%20challenge.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "i have to do summer school for a reason y'know",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/silver/i%20have%20to%20do%20summer%20school%20for%20a%20reason%20y'know.mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/silver.jpg"
+		},
+		{
+			title: "one big chill vibe",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/gingus/one_big_chill_vibe....mp3",
+			meta: "gingus",
+			cover: "https://itzmeunreal.neocities.org/images/covers/gingus/vibe.jpg"
+		}
+	]
+};
---
+++ music/playlist-kmoe.js
@@ -0,0 +1,92 @@
+window.playlistData = {
+	name: "kmoe",
+	pfp: "https://itzmeunreal.neocities.org/images/covers/kmoe/kmoe.png",
+	description: "WHY DO EVERYONE SAMPLE HOMESWITCHER",
+	sectionName: "kmoe",
+	tracks: [
+		{
+			title: "homeswitcher",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/homeswitcher.mp3",
+			meta: "kmoe, Jane Remover",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/homeswitcher.jpg"
+		},
+		{
+			title: "wide eyes",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/wide_eyes.mp3",
+			meta: "kmoe, Jane Remover, juno",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/wide_eyes.jpg"
+		},
+		{
+			title: "dumpster fire",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Dumpster.mp3",
+			meta: "kmoe, bp iv",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "thousand yard stare",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Thousand_yard_stare.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "watershed",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Watershed.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "idiot",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/idiot.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "head first",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Head_first.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "do i keep you up",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Do_I_keep_you_up.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "bloodbath (dance)",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Bloodbath_(Dance).mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "carpet",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Carpet.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "aired out",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Aired.mp3",
+			meta: "kmoe, DAVIN",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "you ruined it",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/You_ruined_it.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "don't be like mouse",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/Don't_be_like_mouse.mp3",
+			meta: "kmoe, DAVIN",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		},
+		{
+			title: "the way i should",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/kmoe/The_way_I_should.mp3",
+			meta: "kmoe",
+			cover: "https://itzmeunreal.neocities.org/images/covers/kmoe/K1.jpg"
+		}
+	]
+};

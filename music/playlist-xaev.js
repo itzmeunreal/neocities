8e8cae634422d8f9cd07f3b1199d0da875bdbb60
unreal! <aimaressi88@gmail.com>
2026-06-18 03:20:53 +0200

    Update playlist-xaev.js

--- music/playlist-xaev.js
+++ music/playlist-xaev.js
@@ -1,68 +1,302 @@
 window.playlistData = {
-    name: "xaev",
-    pfp: "https://itzmeunreal.neocities.org/images/covers/xaev/xaev.jpg",
-    description: "xaev is one of the best complextro/dariacore producers i've ever gotten to listen to. their songs and albums are amazing. please check them out on other streaming services too :0",
-    sectionName: "xaev",
-    tracks: [
-        {
-            title: "it's always a good time",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/it's%20always%20a%20good%20time.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
-        },
-        {
-            title: "mashups aren't real music imagine making them",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/mashups%20aren't%20real%20music%20imagine%20making%20them.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
-        },
-        {
-            title: "the elon musk soundcloud acquisition",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/the%20elon%20musk%20soundcloud%20acquisition.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
-        },
-        {
-            title: "we respawn",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/we%20respawn.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
-        },
-        {
-            title: "insane anime obsessed boyfriend",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/insane%20anime%20obsessed%20boyfriend.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
-        },
-        {
-            title: "WLD U HOLD IT AGAINST ME TURN.UP.THE.BASS Part 117",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/WLD%20U%20HOLD%20IT%20AGAINST%20ME_%20TURN.UP.THE.BASS%20-Part_117.mp3",
-            meta: "xaev, yaxzerothree",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/to_the_core.jpg"
-        },
-        {
-            title: "toby fox discovers the hyperpop soundfont",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/toby%20fox%20discovers%20the%20hyperpop%20soundfont.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
-        },
-        {
-            title: "it's always a good time for complextro",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/it's%20always%20a%20good%20time%20for%20complextro.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
-        },
-        {
-            title: "TAKE-ME-AWAY ~so why try to fight it?~",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/TAKE-ME-AWAY%20~so%20why%20try%20to%20fight%20it_~.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/to_the_core.jpg"
-        },
-        {
-            title: "leroy snares will soon replace NFTs",
-            src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/leroy%20snares%20will%20soon%20replace%20NFTs.mp3",
-            meta: "xaev",
-            cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
-        },
-    ]
+	name: "xaev",
+	pfp: "https://itzmeunreal.neocities.org/images/covers/xaev/xaev.jpg",
+	description: "xaev is one of the best complextro/dariacore producers i've ever gotten to listen to. their songs and albums are amazing. please check them out on other streaming services too :0",
+	sectionName: "xaev",
+	tracks: [
+		{
+			title: "before the story",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/before%20the%20story.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "the elon musk soundcloud acquisition",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/the%20elon%20musk%20soundcloud%20acquisition.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "blisters",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/blisters.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "pronoun picker sing berdlycore",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/pronoun%20picker%20sing%20berdlycore.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "insane anime obsessed boyfriend",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/insane%20anime%20obsessed%20boyfriend.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "homosick",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/homosick.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "DR PEPPER IS THE OLDEST MAJOR SOFT DRINK IN AMERICA",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/DR%20PEPPER%20IS%20THE%20OLDEST%20MAJOR%20SOFT%20DRINK%20IN%20AMERICA.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "tonight's episode of \"most annoying soundcloud tags\" (#jazz & blues)",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/tonight's%20episode%20of%20_most%20annoying%20soundcloud%20tags_%20(%23jazz%20&%20blues).mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "toby fox discovers the hyperpop soundfont",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/toby%20fox%20discovers%20the%20hyperpop%20soundfont.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "a future funk you can't dance out",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/a%20future%20funk%20you%20can't%20dance%20out.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "THEY could be us",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/THEY%20could%20be%20us.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "back to 1999 w omxri",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/back%20to%201999%20w%20omxri.mp3",
+			meta: "xaev, omxri",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "daria math",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/daria%20math.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "the life of an aspiring script kiddie - Bonus track",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/the%20life%20of%20an%20aspiring%20script%20kiddie%20-%20Bonus%20track.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "my friends are so weird - Bonus track",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter3/my%20friends%20are%20so%20weird%20-%20Bonus%20track.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter3.jpg"
+		},
+		{
+			title: "mashups aren't real music imagine making them",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/mashups%20aren't%20real%20music%20imagine%20making%20them.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "leroy snares will soon replace NFTs",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/leroy%20snares%20will%20soon%20replace%20NFTs.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "virtual riot pizza",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/virtual%20riot%20pizza.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "some people need to quit music",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/some%20people%20need%20to%20quit%20music.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "they did surgery on a bird",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/they%20did%20surgery%20on%20a%20bird.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "ok google what the bird doin",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/ok%20google%20what%20the%20bird%20doin.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "none of it matters if we're all gonna fall",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/none%20of%20it%20matters%20if%20we're%20all%20gonna%20fall.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "virgin aspiring VGM musician vs. chad hyperflip producer",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/virgin%20aspiring%20VGM%20musician%20vs.%20chad%20hyperflip%20producer.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "they asked me how we got here, i told them \"sir, this is an arby's\"",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/they%20asked%20me%20how%20we%20got%20here,%20i%20told%20them%20_sir,%20this%20is%20an%20arby's_.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "it's always a good time for complextro",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/it's%20always%20a%20good%20time%20for%20complextro.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "the internet shut down today so that we'd have to listen to berdlycore",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/the%20internet%20shut%20down%20today%20so%20that%20we'd%20have%20to%20listen%20to%20berdlycore.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "his pronouns are they/them",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/his%20pronouns%20are%20they_them.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "unlucky number",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter2/unlucky%20number.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter2.jpg"
+		},
+		{
+			title: "gamers don't die",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/gamers%20don't%20die.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "who wanna share gamer juice",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/who%20wanna%20share%20gamer%20juice.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "u can't look at my priv twitter sorry",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/u%20can't%20look%20at%20my%20priv%20twitter%20sorry.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "i like you OKAY god DAMN!!",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/i%20like%20you%20OKAY%20god%20DAMN!!.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "it's always a good time",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/it's%20always%20a%20good%20time.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "sirens r kinda scary",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/sirens%20r%20kinda%20scary.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "it WAS yo mf birthday but i'm late",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/it%20WAS%20yo%20mf%20birthday%20but%20i'm%20late.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "we respawn",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/we%20respawn.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "what a shame",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/what%20a%20shame.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "call me",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/call%20me.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "i started the UTSC to hyperflip pipeline",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter1/i%20started%20the%20UTSC%20to%20hyperflip%20pipeline.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter1.jpg"
+		},
+		{
+			title: "it gets lonely",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/it%20gets%20lonely.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "here's why some furries are losing it for xaev",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/here's%20why%20some%20furries%20are%20losing%20it%20for%20xaev.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "cookie of a lifetime",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/cookie%20of%20a%20lifetime.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "...i know what you are",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/i%20know%20what%20you%20are.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "explodes pusch3 with my mind",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/explodes%20pusch3%20with%20my%20mind.mp3",
+			meta: "xaev, wylerg",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "i have a 20mm in just listening to hyperpounce",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/i%20have%20a%2020mm%20in%20just%20listening%20to%20hyperpounce.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "paying off (my student loans)",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/paying%20off%20(my%20student%20loans).mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "we're the good guys",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/we're%20the%20good%20guys.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "look idk what to call this song just give me plays",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/look%20idk%20what%20to%20call%20this%20song%20just%20give%20me%20plays.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		},
+		{
+			title: "music sounds WORSE with U",
+			src: "https://file.garden/abIGtIH1yApu4jf7/songs/xaev/chapter0/music%20sounds%20WORSE%20with%20U.mp3",
+			meta: "xaev",
+			cover: "https://itzmeunreal.neocities.org/images/covers/xaev/chapter0.jpg"
+		}
+	]
 };
